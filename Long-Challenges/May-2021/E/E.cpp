9a12ce423b2cbb04cf09ee826dde0e9a731628dd
Manzood <manzood.naqvi@gmail.com>
2021-05-11 17:18:55 +0530

    Added some practice

--- Long-Challenges/May-2021/E/E.cpp
+++ Long-Challenges/May-2021/E/E.cpp
@@ -7,6 +7,17 @@ int32_t main() {
     int t;
     cin >> t;
     while (t--) {
-
+        int n, m;
+        scanf("%lld%lld", &n, &m);
+        int cnt = 0;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                if ((m % j) % i == (m % i) % j) {
+                    cnt++;
+                    printf("%lld %lld\n", i, j);
+                }
+            }
+        }
+        printf("%lld\n", cnt);
     }
 }
