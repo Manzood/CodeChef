1fe0cbb71745c4ba3d09a7ea013af996c0e21c39
Manzood <manzood.naqvi@gmail.com>
2021-06-03 20:48:15 +0530

    Tried E

--- LunchTime/May-2021/E/E.cpp
+++ LunchTime/May-2021/E/E.cpp
@@ -3,6 +3,13 @@ using namespace std;
 #define debug(x) cout << #x << " = " << x << endl;
 #define int long long
 
+int dfs (int node, vector <vector <int>>& adj) {
+    int retval = 0;
+    for (auto x: adj[node]) {
+    }
+    return retval;
+}
+
 int32_t main() {
     int t;
     cin >> t;
@@ -12,33 +19,15 @@ int32_t main() {
         string s;
         cin >> s;
         vector <vector <int>> adj(n);
-        vector <int> parent(n);
+        vector <int> parent(n, -1);
         for (int i = 0; i < n - 1; i++) {
             int p;
             scanf("%lld", &p);
             p--;
-            adj[p].push_back (i + 1);
             parent[i + 1] = p;
+            adj[p].push_back (i + 1);
         }
-        parent[0] = -1;
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '1') {
-                int moves = 0;
-                int tokens = 0;
-                int cur = i;
-                int lastzero = cur;
-                s[cur] = '0';
-                while (parent[cur] != -1) {
-                    moves++;
-                    cur = parent[cur];
-                    if (s[cur] == '1') tokens++;
-                    if (s[cur] == '0') lastzero = cur;
-                }
-                s[lastzero] = '1';
-                ans += moves - tokens;
-            }
-        }
         printf("%lld\n", ans);
     }
 }
