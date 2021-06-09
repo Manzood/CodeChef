dcf7b1fc0da7941dad779f76151e3cc66a63e4d4
Manzood <manzood.naqvi@gmail.com>
2021-06-09 21:44:58 +0530

    Attempted E again

--- LunchTime/May-2021/E/E.cpp
+++ LunchTime/May-2021/E/E.cpp
@@ -3,39 +3,23 @@ using namespace std;
 #define debug(x) cout << #x << " = " << x << endl;
 #define int long long
 
-// find the deepest node from the current node and then swap it, adding distance to answer
-int ans = 0;
+int ans;
 
-pair <int, int> dfs2 (int node, vector <vector <int>>& adj, string s, int dist) {
-    pair <int, int> retval = {-1, -1};
-    if (s[node] == '1') retval = {node, dist};
-    for (auto x: adj[node]) {
-        auto temp = dfs2 (x, adj, s, dist + 1);
-        if (temp.second > retval.second) {
-            retval = temp;
-        }
+int dfs (int node, vector <vector <int>>& adj, string& s) {
+    // returns depth of the deepest token
+    int depth = 0;
+    for (auto u: adj[node]) {
+        depth = max (depth, dfs (u, adj, s));
     }
-    return retval;
+    if (s[node] == '1') {
+        depth++;
+    } else {
+        ans += depth;
+    }
+    return depth;
 }
 
-void dfs (int node, vector <vector <int>>& adj, string& s) {
-    pair <int, int> retval = {-1, -1}; // index, distance
-    if (s[node] == '0') {
-        retval = dfs2 (node, adj, s, 0);
-        // if (node == 0) {
-            // printf("%lld %lld\n", retval.first + 1, retval.second);
-        // }
-        if (retval.second != -1) ans += retval.second;
-        if (retval.first != -1) swap (s[node], s[retval.first]);
-    }
-    for (auto x: adj[node]) {
-        dfs (x, adj, s);
-    }
-    // return the index of the token at the largest distance from here
-    // return retval;
-}
-
-int32_t main() {
+int32_t main () {
     int t;
     cin >> t;
     while (t--) {
@@ -45,13 +29,10 @@ int32_t main() {
         string s;
         cin >> s;
         vector <vector <int>> adj(n);
-        vector <int> parent(n, -1);
         for (int i = 0; i < n - 1; i++) {
             int p;
             scanf("%lld", &p);
-            p--;
-            parent[i + 1] = p;
-            adj[p].push_back (i + 1);
+            adj[--p].push_back (i + 1);
         }
         dfs (0, adj, s);
         printf("%lld\n", ans);
--- LunchTime/May-2021/E/E_backup.cpp
+++ LunchTime/May-2021/E/E_backup.cpp
@@ -3,42 +3,57 @@ using namespace std;
 #define debug(x) cout << #x << " = " << x << endl;
 #define int long long
 
+// find the deepest node from the current node and then swap it, adding distance to answer
+int ans = 0;
+
+pair <int, int> dfs2 (int node, vector <vector <int>>& adj, string s, int dist) {
+    pair <int, int> retval = {-1, -1};
+    if (s[node] == '1') retval = {node, dist};
+    for (auto x: adj[node]) {
+        auto temp = dfs2 (x, adj, s, dist + 1);
+        if (temp.second > retval.second) {
+            retval = temp;
+        }
+    }
+    return retval;
+}
+
+void dfs (int node, vector <vector <int>>& adj, string& s) {
+    pair <int, int> retval = {-1, -1}; // index, distance
+    if (s[node] == '0') {
+        retval = dfs2 (node, adj, s, 0);
+        // if (node == 0) {
+            // printf("%lld %lld\n", retval.first + 1, retval.second);
+        // }
+        if (retval.second != -1) ans += retval.second;
+        if (retval.first != -1) swap (s[node], s[retval.first]);
+    }
+    for (auto x: adj[node]) {
+        dfs (x, adj, s);
+    }
+    // return the index of the token at the largest distance from here
+    // return retval;
+}
+
 int32_t main() {
     int t;
     cin >> t;
     while (t--) {
+        ans = 0;
         int n;
         scanf("%lld", &n);
         string s;
         cin >> s;
         vector <vector <int>> adj(n);
-        vector <int> parent(n);
+        vector <int> parent(n, -1);
         for (int i = 0; i < n - 1; i++) {
             int p;
             scanf("%lld", &p);
             p--;
-            adj[p].push_back (i + 1);
             parent[i + 1] = p;
+            adj[p].push_back (i + 1);
         }
-        parent[0] = -1;
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '1') {
-                int moves = 0;
-                int tokens = 0;
-                int cur = i;
-                int lastzero = cur;
-                s[cur] = '0';
-                while (parent[cur] != -1) {
-                    moves++;
-                    cur = parent[cur];
-                    if (s[cur] == '1') tokens++;
-                    if (s[cur] == '0') lastzero = cur;
-                }
-                s[lastzero] = '1';
-                ans += moves - tokens;
-            }
-        }
+        dfs (0, adj, s);
         printf("%lld\n", ans);
     }
 }
