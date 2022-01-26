662581ebc046db4efcf2d7e7fd7cea682e36ace4
Manzood <manzood.naqvi@gmail.com>
2022-01-27 01:19:49 +0530

    Added a starters problem before I decided to skip the contest

---
+++ Starters/Unrated/Notes.md
@@ -0,0 +1,3 @@
+I started taking part in this round because I assumed it would be rated for div 2 like the other ones.
+However, after finding that problem A was ridiculously simple, I quickly discovered that the contest wasn't rated for me, so I quit.
+My rank, however, was 63 by the time I'd decided to quit.
---
+++ Starters/Unrated/Starters-23/A/A.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+
+#ifdef local
+#include "custom/debugger.h"
+#else
+#define debug(...) 42;
+#endif
+
+using namespace std;
+#define int long long
+
+void solve() {
+    int x;
+    scanf("%lld", &x);
+    printf("%lld %lld %lld\n", x - 1, x, x + 1);
+}
+
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    for (int tt = 1; tt <= t; tt++) {
+        solve();
+    }
+}
