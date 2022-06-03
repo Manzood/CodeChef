bfcfe2b6728ceb1b470a77d5eee252ec968133c9
Manzood <manzood.naqvi@gmail.com>
2022-06-03 15:49:25 +0530

    Attempted E

--- Starters/Starters-41/E/E.cpp
+++ Starters/Starters-41/E/E.cpp
@@ -9,45 +9,67 @@
 using namespace std;
 #define int long long
 
-vector <bool> visited;
-int f(int node, int col, vector <vector <int>>& adj, vector <int>& force, vector <int>& a, vector <int>& b) {
-    int retval = 0;
-    visited[node] = true;
-    bool has_children = false;
-    for (auto u: adj[node]) {
-        if (!visited[u]) {
-            has_children = true;
-            retval += f(u, b[node], adj, force, a, b);
-        }
+constexpr int inf = (int)1e9 + 7;
+
+vector<vector <bool>> visited;
+int dfs(int node, vector<vector<int>>& adj, vector<vector<int>>& dp, int color, vector<int>& a, vector<int>& b, int parent) {
+    int retval = inf;
+    if (visited[node][color]) return 0;
+    visited[node][color] = true;
+    dp[node][0] = 0; dp[node][1] = 0; dp[node][2] = 0;
+    if (b[node] == 1) dp[node][0] = inf;
+    if (b[node] == 0) dp[node][1] = inf;
+    if (a[node] != b[node]) dp[node][2] = inf;
+    if (adj[node].size() == 1) {
+        dp[node][0] = b[node] == 0 ? 1 : inf;
+        dp[node][1] = b[node] == 1 ? 1 : inf;
+        if (color == 2) dp[node][2] = b[node] == a[node] ? 0LL : 1LL;
+        else dp[node][2] = b[node] == color ? 0LL : 1LL;
     }
-    if (b[node] != col) retval++;
-    if (b[node] == col && has_children) retval++;
-    force[node] = retval;
+    for (auto u: adj[node]) {
+        if (u == parent) continue;
+        // if (b[node] == 0) dp[node][0] += min(dp[node][0], 1 + dfs(u, adj, dp, 0, a, b, node));
+        // if (b[node] == 1) dp[node][1] = min(dp[node][1], 1 + dfs(u, adj, dp, 1, a, b, node));
+        // if (b[node] == color || color == 2) dp[node][2] = min(dp[node][2], dfs(u, adj, dp, 2, a, b, node));
+        if (b[node] == 0) dp[node][0] += 1 + dfs(u, adj, dp, 0, a, b, node);
+        if (b[node] == 1) dp[node][1] += 1 + dfs(u, adj, dp, 1, a, b, node);
+        if (b[node] == color || (color == 2 && a[node] == b[node])) dp[node][2] += dfs(u, adj, dp, 2, a, b, node);
+    }
+    retval = min(dp[node][0], min(dp[node][1], dp[node][2]));
     return retval;
 }
-
-// write a dfs
 
 void solve([[maybe_unused]] int test) {
     int n;
     scanf("%lld", &n);
     vector<int> a(n), b(n);
-    vector <vector <int>> adj(n);
+    vector<vector<int>> adj(n);
     for (int i = 0; i < n; i++) {
         scanf("%lld", &a[i]);
+        a[i] ^= 1;
     }
     for (int i = 0; i < n; i++) {
         scanf("%lld", &b[i]);
+        b[i] ^= 1;
     }
     for (int i = 0; i < n - 1; i++) {
         int u, v;
         scanf("%lld%lld", &u, &v);
-        u--; v--;
+        u--;
+        v--;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    vector <int> force(n, 0);
-    f(0, -1, adj, force, a, b);
+    vector<vector<int>> dp(n, vector<int>(3, inf)); // dp[i][0] for black, dp[i][1] for white, dp[i][2] for unchanged
+    visited.resize(n);
+    visited.assign(n, vector <bool> (3, false));
+    int ans = dfs(0, adj, dp, 2, a, b, -1);
+    printf("%lld\n", ans);
+    for (int i = 0; i < n; i++) {
+        if (test == 1) {
+            debug(i, dp[i][0], dp[i][1], dp[i][2]);
+        }
+    }
 }
 
 int32_t main() {
